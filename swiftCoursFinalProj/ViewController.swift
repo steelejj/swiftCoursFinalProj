27661d85de3aadeba34204976c0eef719af9a1ac
James Steele <jwsteele1@me.com>
2019-04-27 20:26:56 -0400

    Initial Commit

---
+++ swiftCoursFinalProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swiftCoursFinalProj
+//
+//  Created by James Steele on 4/27/19.
+//  Copyright © 2019 James Steele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
